1154d26aec2a23c25648ef7ca223b792638782a7
lucas <lucas@pop-os.localdomain>
2024-02-01 21:24:09 -0300

    novas att

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 
-from dotenv import load_dotenv
+
 from fastapi import FastAPI, HTTPException
 import uvicorn
 from pymongo import MongoClient
 from pydantic import BaseModel
 from bson import ObjectId, json_util
 from typing import List, Optional
-import json
+
 
 
 app = FastAPI()
@@ -17,26 +17,28 @@ db = client["treino"]
 
 # cria usuario
 @app.post("/user", response_model=dict )
-def create_user(user:str, sex: str, days_week:int, focus_group:str = None):
+def create_user(user:str, sex: str):
     # Verificar se o usuário já existe
-    if db.users.find_one({"user": user, "sex": sex, "days_week":days_week, "focus_group":focus_group  }):
+    if db.users.find_one({"user": user}):
         raise HTTPException(status_code=400, detail="User already registered")
 
     # jogar usuário no mongo
-    user_data = {"user": user, "sex": sex, "days_week":days_week, "focus_group":focus_group  }
+    user_data = {"user": user, "sex": sex }
     result = db.users.insert_one(user_data)
     raise HTTPException(status_code=200, detail="User registered")
     
     # retorna os dados
     return {"id": str(result.inserted_id), **user_data}
-@app.get("/")
+@app.get("/user")
 def get_users():
     # Retorna informações sobre todos os usuários no banco de dados
-    #precisei tirar o id da lista pois tal quando trazia ele não conseguia iterar
-    users = list(db.users.find({}, {'_id': False}))
+    
+    users = list(db.users.find({}))
+    for user in users:
+        user["_id"] = str(user["_id"])
     return users
 
-@app.delete("/", response_model=dict)
+@app.delete("/user", response_model=dict)
 def delete_user(user: str):
     # faz a verificação pra ver se o usuário existe
     existing_user = db.users.find_one({"user": user})
@@ -47,48 +49,50 @@ def delete_user(user: str):
     return {"message": f"User {user} deleted successfully"}
 
 
-@app.put("/", response_model=dict)
-def update_user(user:str, sex: str = None, days_week:int =None, focus_group:str=None):
+@app.put("/user", response_model=dict)
+def update_user(user:str, sex: str = None):
     
     existing_user = db.users.find_one({"user": user})
     if not existing_user:
         raise HTTPException(status_code=404, detail=f"User {user} not found")
 
     
-    updated_data = {"$set": {"user": user, "sex": sex, "days_week":days_week, "focus_group":focus_group}}
+    updated_data = {"$set": {"user": user, "sex": sex}}
     db.users.update_one({"user": user}, updated_data)
 
     
-    return {"user": user, "sex": sex, "days_week":days_week, "focus_group":focus_group}
+    return {"user": user, "sex": sex}
 
     #-----------------------------------------------//----------------------------------------------------------------------------------------------
 
     #endpoint para criar planilha de treino
 @app.post("/spreadsheet", response_model=dict)
-def create_spreadsheet(spreadsheet: str, sex: str, days_week: int, focus_muscle:str=None ):
-    if db.muscle.find_one({"spreadsheet": spreadsheet, "sex":sex, "days_week":days_week, "focus_muscle":focus_muscle }):
+def create_spreadsheet(spreadsheet: str, user:str, days_week: int, focus_muscle:str=None ):
+    if db.muscle.find_one({"spreadsheet": spreadsheet,"user":user, "sex":sex, "days_week":days_week, "focus_muscle":focus_muscle }):
         raise HTTPException(status_code=400, detail="spreadsheet already registered")
-    spreadsheet_data = {"spreadsheet": spreadsheet, "sex":sex, "days_week":days_week, "focus_muscle":focus_muscle }
+    spreadsheet_data = {"spreadsheet": spreadsheet,"user":user, "days_week":days_week, "focus_muscle":focus_muscle }
     spreadsheet_result = db.muscle.insert_one(spreadsheet_data)
     raise HTTPException(status_code=200, detail="spreadsheet registered")
 
-    return {"id": str(spreadsheet_result.inserted_id), **spreadsheet_data}
+    
 
 @app.get("/spreadsheet")
 def read_spreadsheet():
-    spreadsheets = list(db.muscle.find({}, {'_id': False}))
+    spreadsheets = list(db.muscle.find({}, {'id': False}))
+
+
     return spreadsheets
 
 @app.put("/spreadsheet", response_model=dict)
-def update_spreadsheet(spreadsheet: str=None, sex: str=None, days_week: int=None, focus_muscle:str=None):
+def update_spreadsheet(spreadsheet: str=None,  user: str=None, days_week: int=None, focus_muscle:str=None):
     existing_spreadsheet = db.muscle.find_one ({"spreadsheet": spreadsheet})
     if not existing_spreadsheet:
         raise HTTPException(status_code=400, detail=f"spreadsheet {spreadsheet} not found")
-    updated_spreadsheet = {"$set": {"spreadsheet": spreadsheet, "sex":sex, "days_week":days_week, "focus_muscle":focus_muscle}}
+    updated_spreadsheet = {"$set": {"spreadsheet": spreadsheet, "user": user, "days_week":days_week, "focus_muscle":focus_muscle}}
     db.muscle.update_one({"spreadsheet": spreadsheet}, updated_spreadsheet)
 
     
-    return {"spreadsheet": spreadsheet, "sex":sex, "days_week":days_week, "focus_muscle":focus_muscle}
+    return {"spreadsheet": spreadsheet, "user": user, "days_week":days_week, "focus_muscle":focus_muscle}
 
 @app.delete("/spreadsheet", response_model=dict)
 def delete_spreadsheet(spreadsheet:str):
@@ -102,7 +106,7 @@ def delete_spreadsheet(spreadsheet:str):
 
 
 # if __name__ == '__main__':
-#     load_dotenv()
+    
 #     uvicorn.run(app, host='0.0.0.0', port=8000)
 
 
